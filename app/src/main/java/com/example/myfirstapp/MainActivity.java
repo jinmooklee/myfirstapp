86027a891664a8b58a7dd57cc3fdd58f99de7e97
Jin Mook Lee <nolimitgc@gmail.com>
2021-11-02 23:16:02 +0900

    나의 첫번째 git 커밋

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private final long FINISH_INTERVAL_TIME = 2000;
+    private long backPressedTime = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** 뒤로가기 키 두 번 눌러야 종료되게 하기 */
+    @Override
+    public void onBackPressed() {
+        long tempTime = System.currentTimeMillis();
+        long intervalTime = tempTime - backPressedTime;
+
+        if (0 <= intervalTime && FINISH_INTERVAL_TIME >= intervalTime) {
+            super.onBackPressed();
+        }
+        else {
+            backPressedTime = tempTime;
+            Toast.makeText(this, R.string.confirm_exit, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /** Called with the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DIsplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
